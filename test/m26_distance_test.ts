23814522078921301175ff26f78870983fa4286b
Chris Joakim <christopher.joakim@gmail.com>
2015-05-09 07:26:20 -0400

    Stubbed-out new file m26_distance_test.ts

--- test/m26_distance_test.ts
+++ test/m26_distance_test.ts
@@ -11,27 +11,27 @@ import chai = require('chai');
 import m26  = require('../lib/m26');
 
 var expect = chai.expect;
-var M26Age = m26.M26Age;
+var M26Distance  = m26.M26Distance;
 
-describe('Class M26Age:', () => {
+describe('Class M26Distance:', () => {
 
   describe('constructor', () => {
 
     it('constructor should receive a string arg', (done) => {
-      var a = new M26Age('55.5');
+      var a = new M26Distance('55.5');
       expect(a.val()).to.be.closeTo(55.5, 0.0000000001);
       done();
     });
 
     it('constructor should receive a number arg', (done) => {
-      var a = new M26Age(39.9);
+      var a = new M26Distance(39.9);
       expect(a.val()).to.be.closeTo(39.9, 0.0000000001);
       done();
     });
 
     it('constructor should handle null and undefined', (done) => {
-      var n = new M26Age(null);
-      var u = new M26Age(undefined);
+      var n = new M26Distance(null);
+      var u = new M26Distance(undefined);
       expect(n.val()).to.be.closeTo(0.0, 0.0000000001);
       expect(u.val()).to.be.closeTo(0.0, 0.0000000001);
       done();
@@ -41,38 +41,23 @@ describe('Class M26Age:', () => {
   describe('adding and subtracting', () => {
 
     it('should add two ages', (done) => {
-      var a16 = new M26Age('16.9');
-      var a57 = new M26Age('57.1');
+      var a16 = new M26Distance('16.9');
+      var a57 = new M26Distance('57.1');
       var sum = a57.add(a16);
       expect(sum).to.be.closeTo(74.0, 0.0000000001);
       done();
     });
 
     it('should subtract one age from another', (done) => {
-      var a16 = new M26Age('16.9');
-      var a57 = new M26Age('57.1');
+      var a16 = new M26Distance('16.9');
+      var a57 = new M26Distance('57.1');
       var diff = a57.subtract(a16);
       expect(diff).to.be.closeTo(40.2, 0.0000000001);
       done();
     });
   });
 
-  describe('max pulse calculation', () => {
 
-    it('should implement max_pulse()', (done) => {
-      var a16 = new M26Age(16);
-      var a20 = new M26Age('20');
-      var a21 = new M26Age(21);
-      var a36 = new M26Age(36);
-      var a57 = new M26Age('57');
-      expect(a16.max_pulse()).to.be.closeTo(200.0, 0.0000000001);
-      expect(a20.max_pulse()).to.be.closeTo(200.0, 0.0000000001);
-      expect(a21.max_pulse()).to.be.closeTo(199.0, 0.0000000001);
-      expect(a36.max_pulse()).to.be.closeTo(184.0, 0.0000000001);
-      expect(a57.max_pulse()).to.be.closeTo(163.0, 0.0000000001);
-      done();
-    });
-  });
 
 });
 
